6c3f8d31aef9513ed27de31929ae42c9297464b5
Sanio <BRude602@gmail.com>
2023-01-22 15:49:45 -0500

    first commit eyo

--- main.lua
+++ main.lua
@@ -2,3 +2,7 @@ local mod = RegisterMod("DND", 1)
 DnDMod = mod
 local game = Game()
 local sound = SFXManager()
+
+local function KidNamedMrSeemsGood()
+	print("sanio dont add the eevee burger to the mod dont do it")
+end
